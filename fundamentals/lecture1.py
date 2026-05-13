499b7c8d9cb5a035b71145d75fd8576a591348cd
mayurwabale9 <mayurwabale52@gmail.com>
2026-05-13 17:01:41 +0530

    First commit Done

---
+++ fundamentals/lecture1.py
@@ -0,0 +1,71 @@
+# single line comment 
+'''
+Multi Line Comment 
+
+1.jfkln
+2.EFSDF
+3.SDVV
+4.SDFS
+
+'''
+'''
+Question: Ask User To Enter Two Number and do the addition of two numbers
+
+Logic:
+1.I will ask to user to enter two numbers 
+2.two number will assigned to two variables
+3.one variable : ans = variable1 + variable2
+I will print The Answer
+
+Execution: 
+
+1. input() : will take user input as string and return it back to caller function  (main) as string type data tyep object 
+2.Assign 
+3.
+'''
+# %%
+
+# ask user to enter two numbers 
+variable1 = int(input("Enter the first Number"))
+variable2 = int(input("Enter the second Number"))
+
+ans = variable1 + variable2
+print(ans)
+
+print(type(ans))
+
+name = input("enter the name: ")
+
+print(type(name))
+
+# %%
+# date type of python programing 
+'''
+1.integer
+2.float
+3.string 
+4.boolan
+5.list 
+6.tuple
+7.sets
+8.dictionary
+
+'''
+num = 100
+num1,num2 = 100,200
+
+print(num1,num2)
+
+
+pi = 3.421
+ 
+name = 'india'
+
+india,pak,usa = 100,200,300,'usaa'
+
+#The boolean is always in camalcasing
+a = True
+b = False
+
+print(a and b)
+
